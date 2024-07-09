11154feb17b78f945e30266dc0315ec2906ddd8b
Soyl <Soylgr@yandex.ru>
2024-07-09 18:09:06 +0300

    New

---
+++ src/main/java/com/example/SoyChat/SoyChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoyChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoyChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoyChatApplication.class, args);
+	}
+
+}
